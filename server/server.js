66d9c1523cf26f73532df440e6f3680b8776342e
nodox <snatera09@gmail.com>
2017-07-12 22:08:43 -0500

    apply lint rules to server file

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var helmet = require('helmet');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const index = require('./routes/index');
+const vehicles = require('./routes/vehicles');
+const swaggerJSDoc = require('swagger-jsdoc');
 
 const options = {
   swaggerDefinition: {
@@ -18,22 +20,19 @@ const options = {
   },
   apis: ['./routes/**/*.js'],
 };
-var swaggerJSDoc = require('swagger-jsdoc');
-var swaggerSpec = swaggerJSDoc(options);
-
-// Routes
-var index = require('./routes/index');
-var vehicles = require('./routes/vehicles');
 
 
-var app = express();
+const swaggerSpec = swaggerJSDoc(options);
+
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 
-app.use(helmet())
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -41,34 +40,25 @@ if (process.env.NODE_ENV !== 'test') {
   app.use(logger('dev'));
 }
 
-// Serve static assets
-app.use(express.static(path.resolve(__dirname, '..', 'build')));
-// app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', index);
 app.use('/vehicles', vehicles);
 
-app.get('/swagger.json', function(req, res) {
+app.get('/swagger.json', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
   res.send(swaggerSpec);
 });
 
-// Always return the main index.html, so react-router render the route in the client
-// app.use('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
-// });
-
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -77,7 +67,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
 
 
 module.exports = app;
