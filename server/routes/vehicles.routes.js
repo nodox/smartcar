bf8bab5ed17b4fdf230f79b09e7bba5f3c76c8e4
nodox <snatera09@gmail.com>
2017-07-12 22:49:14 -0500

    lint files in server

--- server/routes/vehicles.routes.js
+++ server/routes/vehicles.routes.js
@@ -3,7 +3,7 @@ const vehicleController = require('../controllers/vehicles.controller');
 
 const router = express.Router();
 
-
+// Routes
 router.get('/:id', vehicleController.getVehiclesById);
 router.get('/:id/doors', vehicleController.getVehiclesDoorsStatus);
 router.get('/:id/fuel', vehicleController.getVehiclesFuelLevel);
--- server/server.js
+++ server/server.js
@@ -20,7 +20,6 @@ const options = {
   apis: ['./routes/**/*.js'],
 };
 
-
 const swaggerSpec = swaggerJSDoc(options);
 const app = express();
 
@@ -40,14 +39,12 @@ app.get('/swagger.json', (req, res) => {
   res.send(swaggerSpec);
 });
 
-
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
-
 
 // error handler
 app.use((err, req, res) => {
@@ -59,6 +56,5 @@ app.use((err, req, res) => {
   res.status(err.status || 500);
   res.render('error');
 });
-
 
 module.exports = app;
