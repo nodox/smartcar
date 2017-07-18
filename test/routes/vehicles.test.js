7ddbc36e3dd46ef8da371ed8c7903db858ee873e
nodox <snatera09@gmail.com>
2017-07-18 14:29:38 -0500

    add correct file

--- test/routes/vehicles.test.js
+++ test/routes/vehicles.test.js
@@ -1,202 +1,155 @@
-//Require the dev-dependencies
-process.env.NODE_ENV = 'test';
+/* eslint-env mocha, es6 */
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../../server/server');
 
-
-let chai = require('chai');
-let chaiHttp = require('chai-http');
-let server = require('../../server/server');
-let should = chai.should();
-let assert = chai.assert;
-let expect = chai.expect;
-
-
+chai.should();
 chai.use(chaiHttp);
 chai.use(require('chai-things'));
 
+process.env.NODE_ENV = 'test';
+
+const validId = 1234;
+const validIdOfCarWithTwoDoors = 1235;
+const invalidId = 'xxx';
+const unknownId = -999;
 
 describe('GM Vehicles API', () => {
-
-  /**
-   * Test the /GET route
-   */
   describe('GET /vehicles/:id', () => {
     it('should GET a single vehicle object by id', (done) => {
-      let id = 1234;
-      let url = '/vehicles/' + id.toString();
+      const url = `/vehicles/${validId.toString()}`;
       chai.request(server)
         .get(url)
         .end((err, res) => {
-            res.should.have.status(200);
-            res.body.should.have.property('vin');
-            res.body.should.have.property('color');
-            res.body.should.have.property('doorCount');
-            res.body.should.have.property('driveTrain');
+          res.should.have.status(200);
+          res.body.should.have.property('vin');
+          res.body.should.have.property('color');
+          res.body.should.have.property('doorCount');
+          res.body.should.have.property('driveTrain');
           done();
         });
     });
 
     it('should return empty, when using an unknown id', (done) => {
-      let unknownId = -999;
-      let url = '/vehicles/' + unknownId.toString();
+      const url = `/vehicles/${unknownId.toString()}`;
       chai.request(server)
         .get(url)
         .end((err, res) => {
-            res.should.have.status(400);
-            res.body.should.be.empty;
-          done();
-        });
-    });
-  
-    it('should return empty, when using an invalid id', (done) => {
-      let invalidId = "xxx";
-      let url = '/vehicles/' + invalidId.toString();
-      chai.request(server)
-        .get(url)
-        .end((err, res) => {
-            res.should.have.status(400);
-            res.body.should.be.empty;
+          res.should.have.status(400);
+          res.body.should.be.empty;
           done();
         });
     });
 
+    it('should return empty, when using an invalid id', (done) => {
+      const url = `/vehicles/${invalidId.toString()}`;
+      chai.request(server)
+        .get(url)
+        .end((err, res) => {
+          res.should.have.status(400);
+          res.body.should.to.have.empty;
+          done();
+        });
+    });
   });
 
-
   describe('GET /vehicles/:id/doors', () => {
-
     describe('vehicles with two doors', () => {
       it('should return array of size 2', (done) => {
-        let id = 1235;
-        let url = '/vehicles/' + id.toString() +'/doors';
-
+        const url = `/vehicles/${validIdOfCarWithTwoDoors.toString()}/doors`;
         chai.request(server)
           .get(url)
           .end((err, res) => {
-              res.should.have.status(200);
-              res.body.should.be.an('array').that.has.lengthOf(2);
-
+            res.should.have.status(200);
+            res.body.should.be.an('array').that.has.lengthOf(2);
             done();
           });
       });
-
     });
 
     describe('vehicles with four doors', () => {
-
       it('should return array of size 4', (done) => {
-        let id = 1234;
-        let url = '/vehicles/' + id.toString() +'/doors';
-
+        const url = `/vehicles/${validId.toString()}/doors`;
         chai.request(server)
           .get(url)
           .end((err, res) => {
-              res.should.have.status(200);
-              res.body.should.be.an('array').that.has.lengthOf(4);
+            res.should.have.status(200);
+            res.body.should.be.an('array').that.has.lengthOf(4);
             done();
           });
-      });    
-
+      });
     });
 
-
     it('should return an array of doors with locked and location values', (done) => {
-      let id = 1235;
-      let url = '/vehicles/' + id.toString() +'/doors';
-
+      const url = `/vehicles/${validIdOfCarWithTwoDoors.toString()}/doors`;
       chai.request(server)
         .get(url)
         .end((err, res) => {
-            res.should.have.status(200);
-            res.body.should.be.an('array');
-            res.body.should.all.have.property('locked');
-            res.body.should.all.have.property('location');
+          res.should.have.status(200);
+          res.body.should.be.an('array');
+          res.body.should.all.have.property('locked');
+          res.body.should.all.have.property('location');
           done();
         });
     });
-
-
   });
 
 
   describe('GET /vehicles/:id/fuel', () => {
-    let id = 1235;
-    let url = '/vehicles/' + id.toString() + '/fuel';
-
+    const url = `/vehicles/${validIdOfCarWithTwoDoors.toString()}/fuel`;
     it('should return the fuel level', (done) => {
       chai.request(server)
         .get(url)
         .end((err, res) => {
-            res.should.have.status(200);
-            res.body.should.have.property('percent').that.is.a('number');
+          res.should.have.status(200);
+          res.body.should.have.property('percent').that.is.a('number');
           done();
         });
     });
-
-
-
   });
 
-
   describe('GET /vehicles/:id/battery', () => {
-    let id = 1235;
-    let url = '/vehicles/' + id.toString() + '/battery';
-
+    const url = `/vehicles/${validIdOfCarWithTwoDoors.toString()}/battery`;
     it('should return the battery level', (done) => {
       chai.request(server)
         .get(url)
         .end((err, res) => {
-            res.should.have.status(200);
-            res.body.should.have.property('percent').that.is.a('number');
+          res.should.have.status(200);
+          res.body.should.have.property('percent').that.is.a('number');
           done();
         });
     });
-
-
   });
 
   describe('POST /vehicles/:id/engine', () => {
-
-
     describe('START action', () => {
-
-      let id = 1235;
-      let url = '/vehicles/' + id.toString() + '/engine';
-      let data = { action: 'START'};
-
+      const url = `/vehicles/${validIdOfCarWithTwoDoors.toString()}/engine`;
+      const data = { action: 'START' };
       it('should return status', (done) => {
         chai.request(server)
           .post(url)
           .send(data)
           .end((err, res) => {
-              res.should.have.status(200);
-              res.body.should.have.property('status');
+            res.should.have.status(200);
+            res.body.should.have.property('status');
             done();
           });
       });
     });
-
-
 
     describe('STOP action', () => {
-
-      let id = 1234;
-      let url = '/vehicles/' + id.toString() + '/engine';
-      let data = { action: 'STOP'};
-
+      const url = `/vehicles/${validId.toString()}/engine`;
+      const data = { action: 'STOP' };
       it('should return status', (done) => {
         chai.request(server)
           .post(url)
           .send(data)
           .end((err, res) => {
-              res.should.have.status(200);
-              res.body.should.have.property('status');
+            res.should.have.status(200);
+            res.body.should.have.property('status');
             done();
           });
       });
     });
-
-
   });
-
-
 });
